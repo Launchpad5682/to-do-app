00d544a704183bfdacb4cdbd89c5efc82cd854aa
launchpad5682 <launchpad5682@gmail.com>
2021-08-11 10:30:49 +0530

    Input Form implemented

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { TasksContext } from "../context/TasksContext";
 
 function InputForm() {
+  const [tasks, setTasks] = useContext(TasksContext);
+  const [inputTodo, setInputTodo] = useState("");
+
+  function onSubmitHandler(event) {
+    event.preventDefault();
+    setTasks([...tasks, { task: inputTodo, completed: false }]);
+    // console.log(tasks);
+    setInputTodo("");
+  }
+
+  function onChangeHandler(event) {
+    setInputTodo(event.target.value);
+  }
+
   return (
-    <div>
-      <input type="text" />
+    <form onSubmit={onSubmitHandler}>
+      <input type="text" onChange={onChangeHandler} value={inputTodo} />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
 
