7fe1ba76f35a2830287916afe7a775bf98be2843
launchpad5682 <launchpad5682@gmail.com>
2021-08-12 00:21:57 +0530

    getting data implemented

--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -4,11 +4,22 @@ export const TasksContext = createContext();
 
 export const TasksProvider = (props) => {
   /*{ task: null, completed: false} */
-  const [tasks, setTasks] = useState([]);
+
+  const [tasks, setTasks] = useState(getToDoData());
 
   return (
     <TasksContext.Provider value={[tasks, setTasks]}>
       {props.children}
     </TasksContext.Provider>
   );
+};
+
+// getting data from localStorage
+const getToDoData = () => {
+  let toDoData = localStorage.getItem("toDo");
+  if (toDoData) {
+    return JSON.parse(toDoData);
+  } else {
+    return [];
+  }
 };
